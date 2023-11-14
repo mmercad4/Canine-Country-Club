9ce2ad53dbc7d424df4e86e86cbdd3c7318f76b7
Marco Mercado <marco@stonepawtraining.com>
2023-11-14 13:12:48 -0500

    Added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        customPrimary: "#0B1938",
+        customSecondary: "#244C84",
+        customTertiary: "#2596BE",
+        customWhite: "#E8E9ED",
+        customGray: "#4E596E",
+      },
+    },
   },
   plugins: [],
 };
